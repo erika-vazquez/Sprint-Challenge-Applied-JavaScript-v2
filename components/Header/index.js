be5abee8e7bd2f1bee1c494326de924a18d36b9a
erika-vazquez <ito2016@protonmail.ch>
2019-07-12 10:24:09 -0500

    Created a function to build component

--- components/Header/index.js
+++ components/Header/index.js
@@ -8,5 +8,21 @@
 //    <span class="temp">98°</span>
 //  </div >
 // And add it to the DOM in the .headerContainer component
+const header = document.querySelector('.header')
 
-function Header() {}
+function Header(hdr, lambDate, lambTitle, lambTemp) {
+// create the elements
+const headerC = document.createElement('div')
+const date = document.createElement('span')
+const title = document.createElement('h1')
+const temp = document.createElement('span')
+
+// set the styles
+headerC.classList.add('headerC')
+date.classList.add('headerC') 
+title.classList.add('title')
+temp.classList.add('temp')
+
+
+
+}
