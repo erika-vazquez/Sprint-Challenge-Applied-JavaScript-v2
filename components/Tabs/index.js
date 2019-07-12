054ac13ff4516c0291b2453e39f83499a1a81c2f
erika-vazquez <ito2016@protonmail.ch>
2019-07-12 15:33:29 -0500

    Fixed tabs component error

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -8,26 +8,25 @@
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
 
-axios.get('https://lambda-times-backend.herokuapp.com/topics')
-.then( (response) => {
+axios.get('https://lambda-times-backend.herokuapp.com/topics') //GET request 
+.then( (response) => {                   // Handles Success: here's where we get the results from server
     console.log('response', response)   //test
-    const tabs = document.querySelector('.topics')    
-     response.data.topics.forEach( item => {
-      tabs.appendChild(tabsComponent(item))
+    const tabs = document.querySelector('.topics')    //DOM manipulation
+     response.data.topics.forEach( item => {    //get topics inside data array
+      tabs.appendChild(tabsComponent(item)) //appends the tabsComponent value to tabs
     }) 
-    
   })
-  .catch( (err) => {
+   // Handles failure:
+  .catch( (err) => {  
     console.log(err)
   })
 
-
-  
+  //function that accepts a single object as its only argument
+  //Creates a tabs component 
   function tabsComponent(data){
     const tab = document.createElement('div');
     tab.classList.add('tab')
     tab.textContent = data;
-
     
     return tab
   }
